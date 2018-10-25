b190a6533ec5e18ea728c4ec8f1e549d9b10279e
yuandong <837215079@qq.com>
2018-10-25 20:13:27 +0800

    调试

--- public/static/js/functions.js
+++ public/static/js/functions.js
@@ -1,5 +1,11 @@
 (function(window,$){
     window.redream = {
+
+        /**
+         * 获取随机字符串
+         * @param len        字符串长度
+         * @returns {string}
+         */
         randomString:function(len){
             len = len || 32;
             var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
@@ -10,7 +16,15 @@
             }
             return pwd;
         },
+
+        /**
+         * 消息提示框
+         * @param message   提示内容
+         * @param time      消息自动消失时间 (默认 2 秒)
+         */
         showTip:function(message,time){
+
+            var time = time || 2000;
             var idSelector = objClass.randomString(20)+ new Date().getTime();
             $('body').append('<div class="wkf" id="'+ idSelector+'" style="display: block">'+message+'</div>');
             $('#'+ idSelector).show().delay(time).fadeOut();
