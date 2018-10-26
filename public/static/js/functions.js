d2aa87e48df6060aab797309da49d174c7dd2fd4
yuandong <837215079@qq.com>
2018-10-26 10:23:03 +0800

    提交

--- public/static/js/functions.js
+++ public/static/js/functions.js
@@ -23,9 +23,8 @@
          * @param time      消息自动消失时间 (默认 2 秒)
          */
         showTip:function(message,time){
-
             var time = time || 2000;
-            var idSelector = objClass.randomString(20)+ new Date().getTime();
+            var idSelector = this.randomString(20)+ new Date().getTime();
             $('body').append('<div class="wkf" id="'+ idSelector+'" style="display: block">'+message+'</div>');
             $('#'+ idSelector).show().delay(time).fadeOut();
             (function(id,tt){
@@ -33,6 +32,40 @@
                     $('#'+ id).remove();
                 },tt);
             })(idSelector,time);
+        },
+
+        /**
+         * 验证手机号格式
+         * @param mobile
+         * @returns {boolean}
+         */
+        checkMobile:function(mobile){
+            var reg = /^[1][3,4,5,7,8][0-9]{9}$/;
+            return reg.test(mobile);
+        },
+
+        /**
+         * 校验密码：8-16位不为纯数字或字母的正在表达式
+         * @param password
+         * @returns {boolean}
+         */
+        checkPassword:function(password){
+            var patrn=/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{8,16}$/;
+            if (!patrn.exec(password)) return false
+            return true
+        },
+
+        /**
+         * 判断字符串是否为空
+         * @param str        字符串
+         * @returns {boolean}
+         */
+        isEmptyStr:function(str){
+            if(str.length == 0){
+                return true;
+            }
+
+            return false;
         }
     }
 })(window,jQuery);
