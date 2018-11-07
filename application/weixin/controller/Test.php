d45c789552e798de69ced7aba8c2a09db475ec1c
yuandong <837215079@qq.com>
2018-11-07 17:54:57 +0800

    调试

---
+++ application/weixin/controller/Test.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\weixin\controller;
+
+use think\Controller;
+
+use app\api\domain\UserFriendDomain;
+use app\api\domain\FavoriteDomain;
+use app\api\domain\ArticleDomain;
+
+class Test  extends Controller
+{
+    public function test(){
+        /**$res = new UserFriendDomain();
+        halt($res->createFriend(6,5));**/
+
+
+
+//        $model = new FavoriteDomain();
+////        $ss = $model->create([
+////            'user_id'     =>4,
+////            'table_name'  =>'article',
+////            'title'       =>'重庆22路公交坠江前为何没有减速？｜深度聚焦555',
+////            'object_id'   =>38,
+////            'thumbnail'   =>'',
+////        ]);
+//
+//        halt($model->getFavoriteList(4,1,15));
+
+
+//        $model = new ArticleDomain();
+//        $res = $model->getCommentArticle(4);
+//
+//        halt($res);
+    }
+}
