d060ac0bb6e6113e6dc9ca538d58892229f24fa8
wuyonghui <837215079@qq.com>
2018-11-09 17:36:15 +0800

    上传身份证明

---
+++ public/static/js/portrait.js
@@ -0,0 +1,168 @@
+function showPreview(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt").css("opacity","0");
+    }
+}
+function showPreview1(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait1');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt1").css("opacity","0");
+    }
+}
+function showPreview3(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait3');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt3").css("opacity","0");
+    }
+}
+function showPreview4(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait4');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt4").css("opacity","0");
+    }
+}
+function showPreview5(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait5');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt5").css("opacity","0");
+    }
+}
+function showPreview6(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait6');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt6").css("opacity","0");
+    }
+}
+function showPreview7(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait7');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt7").css("opacity","0");
+    }
+}
+function showPreview8(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait8');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt8").css("opacity","0");
+    }
+}
+function showPreview9(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrait9');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-dt9").css("opacity","0");
+    }
+}
+function showPrevi(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrai1');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-d1").css("opacity","0");
+    }
+}
+function showPrevi1(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrai2');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-d2").css("opacity","0");
+    }
+}
+function showPrevi2(source) {
+    var file = source.files[0];
+    if(window.FileReader) {
+        var fr = new FileReader();
+        console.log(fr);
+        var portrait = document.getElementById('portrai3');
+        fr.onloadend = function(e) {
+            portrait.src = e.target.result;
+        };
+        fr.readAsDataURL(file);
+        portrait.style.display = 'block'
+        $(".wl-d3").css("opacity","0");
+    }
+}
