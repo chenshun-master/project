ff5ca1eff3111691772f3022e1bd7ed47f26d45d
yuandong <837215079@qq.com>
2018-11-14 17:53:16 +0800

    调试

--- application/api/domain/UserFriendDomain.php
+++ application/api/domain/UserFriendDomain.php
@@ -170,4 +170,13 @@ class UserFriendDomain
 
         return $isTrue ? true : false;
     }
+
+    public function getPrivateLetterList($user_id,$page,$page_size){
+        return [
+            'rows'          =>0,
+            'page'          =>1,
+            'page_total'    =>1,
+            'total'         =>0
+        ];
+    }
 }
