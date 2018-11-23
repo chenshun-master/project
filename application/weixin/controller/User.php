257bce3c8cf6ddeaa6868624d1d82dcf99596974
yuandong <837215079@qq.com>
2018-11-23 10:09:57 +0800

    爱迪生发送

--- application/weixin/controller/User.php
+++ application/weixin/controller/User.php
@@ -72,7 +72,7 @@ class User extends BaseController
         if(!$this->checkLogin()){
             return redirect('/weixin/index/login');
         }
-        
+
         return $this->fetch('user/friends');
     }
 
