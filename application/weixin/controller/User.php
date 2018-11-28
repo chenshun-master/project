017d1a63677bff7ed2c70f42452b6ef1e29aa8d0
yuandong <837215079@qq.com>
2018-11-28 16:03:03 +0800

    阿斯顿发生

--- application/weixin/controller/User.php
+++ application/weixin/controller/User.php
@@ -242,34 +242,22 @@ class User extends BaseController
     }
 
     /**
-     * 上传用户头像接口
+     * 用户头像修改
      */
-    public function uploadHeadImg(Request $request){
-        $file = request()->file("image");
-        $img_domain = config('conf.file_save_domain');
-
-        #文件上传类型
-        $fileExt   = ['jpg','jpeg','png'];
-        if($file){
-            $size = 1024*1024*3;              #单位字节
-            if(!$file->checkSize($size)){
-                return $this->returnData([],'上传图片大小不能超过3M',302);
-            }
-
-            if(!$file->checkExt($fileExt)){
-                return $this->returnData([],'文件格式错误只支持jpg,jpeg及png格式的图片',303);
-            }
-
-            $info = $file->move( 'uploads/head');
-            if($info){
-                $path_dir = $img_domain.'/uploads/head/'.str_replace("\\","/",$info->getSaveName());
-                return $this->returnData([
-                    'image_url'=>$path_dir
-                ],'图片上传成功',200);
-            }else{
-                return $this->returnData([],$file->getError(),305);
-            }
+    public function uploadHead(Request $request){
+        if(!$this->checkLogin()){
+            return $this->returnData([],'用户未登录',401);
         }
+
+        $img = $request->post('img');
+        $data = base64_decode(str_replace(['data:image/jpeg;base64,',' '],['','+'],$img));
+
+        $img = $this->_userDomain->uploadHead($this->getUserId(),$data);
+        if($img === false){
+            return $this->returnData([],'上传失败',305);
+        }
+
+        return $this->returnData(['imgUrl' =>$img],'上传成功',200);
     }
 
     /**
