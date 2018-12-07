dc5b26d62167c8083ed6dae9bbe67b307eafc555
chenshun <837215079@qq.com>
2018-12-07 17:27:41 +0800

    体骄傲

--- application/api/model/AdminModel.php
+++ application/api/model/AdminModel.php
@@ -8,7 +8,6 @@
 
 namespace app\api\model;
 
-
 use think\Model;
 
 class AdminModel extends Model
@@ -16,23 +15,28 @@ class AdminModel extends Model
     // 设置当前模型对应的完整数据表名称
     protected $table = 'wl_admin';
     /**
-     * 用户登录认证
-     * @param  string  $username 用户名
-     * @param  string  $password 用户密码
-     * @return integer           登录成功
+     * 登录操作
+     * @param $username
+     * @param $password
+     * @param bool $quickLogin   是否需要验证密码
+     * @return array|int|null|\PDOStatement|string|\think\Model     2:用户不存在   3:密码错误
      */
-    public function login($username, $password){
-        $map = array();
-        $map['username'] = $username;
-        /* 获取用户数据 */
-        $user = $this->get($map);
-        if($user){
-            /* 验证用户密码 */
-            if(encryptPwd($password) !== $user->password) {
-                return 2;
-            }
-        } else {
-            return 3; //用户不存在或被禁用
+    public function login($username,$password,$quickLogin=false){
+        $info = self::where('username',$username)->find();
+        if(!$info['username']){
+            return 2;
         }
+
+        #判断是否需要验证登录密码
+        if(!$quickLogin){
+            if($info['password'] !== encryptPwd($password)){
+                return 3;
+            }
+        }
+
+        //登录用户读取属于自己的消息通知(后期会加入异步队列处理)
+        (new \app\api\domain\MessageDomain())->readMsg($info['id']);
+        return $info;
     }
+
 }
