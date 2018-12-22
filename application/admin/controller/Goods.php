b3099c6ff37e91c072823bcda3cf325e72b43463
chenshun <837215079@qq.com>
2018-12-22 11:23:12 +0800

    体骄傲

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -29,8 +29,12 @@ class Goods extends BaseController
 
     /**
      * 商品列表
-     * @param  $page 当前页码
-     * @param  $page_size 每页展示的数据
+     * @param int $page
+     * @param int $page_size
+     * @return false|string
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
      */
     public function getGoodsList($page=1,$page_size=10){
         $status = input('param.status');
