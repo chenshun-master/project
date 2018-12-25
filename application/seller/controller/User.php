a0d8c7d84273dcd422a6fb163342100cf33b5e84
yuandong <837215079@qq.com>
2018-12-25 10:14:43 +0800

    调试

---
+++ application/seller/controller/User.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2018/12/25
+ * Time: 10:08
+ */
+
+namespace app\seller\controller;
+
+
+class User extends BaseController
+{
+
+    public function myDoctor(){
+        
+    }
+
+}
