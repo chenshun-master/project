63e0697f5c3a5ca35dfc72157d6281745b4e6125
yuandong <837215079@qq.com>
2019-01-02 13:28:43 +0800

    调试

--- application/api/controller/Test.php
+++ application/api/controller/Test.php
@@ -19,11 +19,19 @@ class Test
 //        $mobile = $obj->getMobile($user_info,'weixin');
 //
 //        halt($mobile);
-//
-//
-        $str = encryptStr('TEt3AV9wEmP8MTNHTm3L7ehtKYmNbqQ9YBzP58SGJ47gh+qb','D',config('conf.secret_key'));
 
+        $str = encryptStr('TEt3AV9wEmP8MTNHTm3L7ehtKYmNbqQ9YBzP58SGJ47gh+qb','D',config('conf.secret_key'));
         halt($str);
         return redirect('/weixin/index/otherLoginBindingMobile')->params(['auth_token'=>urlencode($str),'type'=>1]);
+    }
+
+    public function tt(){
+
+        //密码加密
+        $str = 'aa123456';
+        $pwd = password_hash('aa123456',PASSWORD_DEFAULT);
+        dump($pwd);
+        dump(strlen($pwd));
+        dump(password_verify( $str, $pwd));
     }
 }
