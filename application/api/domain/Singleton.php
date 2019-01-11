c92a2e63157bdbeedeae5d2af92004a23b926b81
yuandong <837215079@qq.com>
2019-01-11 13:46:29 +0800

    调试

---
+++ application/api/domain/Singleton.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\api\domain;
+
+class Singleton
+{
+
+    public static $list= [
+        'diarydomain'=>'app\\api\\domain\\DiaryDomain',
+    ];
+
+    static public $instance = [];
+
+    public static function getDomain($domainName){
+        if(!isset(self::$instance[strtolower($domainName)])){
+            if(!isset(self::$list[strtolower($domainName)])){
+                throw new \think\Exception('实例化类不存在');
+            }
+
+            return self::$instance[strtolower($domainName)]  = new self::$list[strtolower($domainName)];
+        }else{
+            return self::$instance[strtolower($domainName)];
+        }
+    }
+}
