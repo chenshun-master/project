c961a6a5e8cf8d58f9d81f3179116bdf4dd20174
yuandong <837215079@qq.com>
2019-01-15 18:46:26 +0800

    调试

--- application/api/controller/Test.php
+++ application/api/controller/Test.php
@@ -1,11 +1,13 @@
 <?php
-
 namespace app\api\controller;
 
 use app\api\model\SpCategoryModel;
 use think\Db;
 use app\api\domain\ShOrderDomain;
 use app\api\domain\SpGoodsDomain;
+
+use app\common\classs\MyJwt;
+use app\common\classs\Jwt;
 
 class Test
 {
@@ -16,4 +18,17 @@ class Test
 //            Db::name('sh_order')->where('goods_id',$v['id'])->update(['seller_id'=>$v['user_id']]);
 //        }
     }
+
+
+
+    public function t2(){
+        $payload_test = array('iss' => 'adminsdfasd');
+
+
+
+        halt(MyJwt::getPayload($payload_test));
+
+    }
+
+
 }
---
+++ application/common/classs/MyJwt.php
@@ -0,0 +1,141 @@
+<?php
+namespace app\common\classs;
+
+/**
+ * PHP实现jwt
+ */
+class MyJwt
+{
+    //使用HMAC生成信息摘要时所使用的密钥
+    private static $key = 'wl-webtoken-sha256';
+
+    private static $header = array(
+        'alg' => 'HS256', //生成signature的算法
+        'typ' => 'JWT'    //类型
+    );
+
+    /**
+     * 获取jwt token
+     * @param array $payload jwt载荷  格式如下非必须
+     * [
+     * 'iss'=>'jwt_admin', //该JWT的签发者
+     * 'iat'=>time(), //签发时间
+     * 'exp'=>time()+7200, //过期时间
+     * 'nbf'=>time()+60, //该时间之前不接收处理该Token
+     * 'sub'=>'www.admin.com', //面向的用户
+     * 'jti'=>md5(uniqid('JWT').time()) //该Token唯一标识
+     * ]
+     * @return bool|string
+     */
+    public static function getToken(array $payload)
+    {
+        if (is_array($payload)) {
+            $base64header = self::base64UrlEncode(json_encode(self::$header, JSON_UNESCAPED_UNICODE));
+
+            $base64payload = self::base64UrlEncode(json_encode($payload, JSON_UNESCAPED_UNICODE));
+
+            $token = $base64header . '.' . $base64payload . '.' . self::signature($base64header . '.' . $base64payload, self::$key, self::$header['alg']);
+            return $token;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 存放实际需要传递的数据
+     * @param $uid
+     * @return string
+     */
+    public static  function getPayload($payload)
+    {
+        $detaultPayload = [
+            'iss' => 'admin', //该JWT的签发者
+            'exp' => time() + 60 * 60 * 2, //过期时间
+            'sub' => 'test', //主题
+            'aud' => 'every',//用户
+            'nbf' => time(), //生效时间
+            'iat' => time(), //签发时间
+            'jti' =>md5(uniqid('JWT').time()), //该Token唯一标识
+            'uid' => '',  //私有信息
+        ];
+
+        return array_merge($detaultPayload,$payload);
+    }
+
+    /**
+     * 验证token是否有效,默认验证exp,nbf,iat时间
+     * @param string $Token 需要验证的token
+     * @return bool|string
+     */
+    public static function verifyToken(string $Token)
+    {
+        $tokens = explode('.', $Token);
+        if (count($tokens) != 3)
+            return false;
+
+        list($base64header, $base64payload, $sign) = $tokens;
+
+        //获取jwt算法
+        $base64decodeheader = json_decode(self::base64UrlDecode($base64header), JSON_OBJECT_AS_ARRAY);
+        if (empty($base64decodeheader['alg']))
+            return false;
+
+        //签名验证
+        if (self::signature($base64header . '.' . $base64payload, self::$key, $base64decodeheader['alg']) !== $sign)
+            return false;
+
+        $payload = json_decode(self::base64UrlDecode($base64payload), JSON_OBJECT_AS_ARRAY);
+
+        //签发时间大于当前服务器时间验证失败
+        if (isset($payload['iat']) && $payload['iat'] > time())
+            return false;
+
+        //过期时间小宇当前服务器时间验证失败
+        if (isset($payload['exp']) && $payload['exp'] < time())
+            return false;
+
+        //该nbf时间之前不接收处理该Token
+        if (isset($payload['nbf']) && $payload['nbf'] > time())
+            return false;
+
+        return $payload;
+    }
+
+
+    /**
+     * base64UrlEncode  https://jwt.io/ 中base64UrlEncode编码实现
+     * @param string $input 需要编码的字符串
+     * @return string
+     */
+    private static function base64UrlEncode(string $input)
+    {
+        return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
+    }
+
+    /**
+     * base64UrlEncode https://jwt.io/ 中base64UrlEncode解码实现
+     * @param string $input 需要解码的字符串
+     * @return bool|string
+     */
+    private static function base64UrlDecode(string $input)
+    {
+        return base64_decode(str_pad(strtr($input, '-_', '+/'), strlen($input) % 4, '=', STR_PAD_RIGHT));
+    }
+
+    /**
+     * HMACSHA256签名  https://jwt.io/ 中HMACSHA256签名实现
+     * @param string $input 为base64UrlEncode(header).".".base64UrlEncode(payload)
+     * @param string $key
+     * @param string $alg 算法方式
+     * @return mixed
+     */
+    private static function signature(string $input, string $key, $alg = 'HS256')
+    {
+        $alg_config = array(
+            'HS256' => 'sha256',
+            'HS384' => 'sha384'
+        );
+
+        return self::base64UrlEncode(hash_hmac($alg_config[$alg], $input, $key, true));
+    }
+}
