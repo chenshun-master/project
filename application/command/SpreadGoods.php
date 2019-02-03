bee06ceaf041716d3007947521c2a26751cd30e0
yuandong <837215079@qq.com>
2019-02-03 21:41:34 +0800

    调试

--- application/command/SpreadGoods.php
+++ application/command/SpreadGoods.php
@@ -21,11 +21,14 @@ class SpreadGoods extends Command
     protected function execute(Input $input, Output $output)
     {
         $time = date('Y-m-d 00:00:00',strtotime('-7 day'));
+        $field = ['spread.id','spread.uid','spread.type','spread.num','order.status as order_status'];
+
         $infos = Db::name('sp_spread_record')->alias('spread')
             ->join('sh_order order','order.id = spread.order_id')
-            ->where('spread.status',1)->field(['spread.id','spread.uid','spread.type','spread.num'])
+            ->where('spread.status',1)->field($field)
             ->where('pay_time', '<= time', $time)
             ->select();
+
 
         if(!$infos){
             $output->writeln("No data detected");exit;
@@ -38,30 +41,54 @@ class SpreadGoods extends Command
                     throw new \think\Exception('查询分销用户信息失败');
                 }
 
-                /** 分销产品赠送积分*/
-                if (!Db::name('user')->where('id', $data['uid'])->setInc('score', intval($data['num']))) {
-                    throw new \think\Exception('更新账户积分失败');
+                if(in_array($data['order_status'],[3,5])){
+                    if (!Db::name('user')->where('id', $data['uid'])->inc('score',$data['num'])->dec('lock_score',$data['num'])->update()) {
+                        throw new \think\Exception('更新账户积分失败');
+                    }
+
+                    $scoreRecord = [
+                        'user_id' => $data['uid'],
+                        'status' => 1,
+                        'type' => 1,
+                        'score' => intval($data['num']),
+                        'score_front' => $uInfo['score'],
+                        'score_after' => $uInfo['score'] + intval($data['num']),
+                        'remarks'    =>'分销商品 - 冻结积分解冻',
+                        'created_time' => date('Y-m-d H:i:s')
+                    ];
+
+                    if (!$getId = Db::name('score_record')->insertGetId($scoreRecord)) {
+                        throw new \think\Exception('添加积分记录失败');
+                    }
+
+                    if(!Db::name('sp_spread_record')->where('id',$data['id'])->where('status',1)->update(['status'=>2,'thaw_record_id'=>$getId])){
+                        throw new \think\Exception('分销产品兑付记录表跟新失败');
+                    }
                 }
+                else{
+                    if (!Db::name('user')->where('id', $data['uid'])->dec('lock_score',$data['num'])->update()) {
+                        throw new \think\Exception('更新账户积分失败');
+                    }
 
-                $scoreRecord = [
-                    'user_id' => $data['uid'],
-                    'status' => 1,
-                    'type' => 1,
-                    'score' => intval($data['num']),
-                    'score_front' => $uInfo['score'],
-                    'score_after' => $uInfo['score'] + intval($data['num']),
-                    'remarks'    =>'商品分销赠送积分奖励',
-                    'created_time' => date('Y-m-d H:i:s')
-                ];
+                    $scoreRecord = [
+                        'user_id' => $data['uid'],
+                        'status' => 0,
+                        'type' => 2,
+                        'score' => intval($data['num']),
+                        'score_front' => $uInfo['score'],
+                        'score_after' => $uInfo['score'] + intval($data['num']),
+                        'remarks'    =>'分销商品 - 退回冻结积分',
+                        'created_time' => date('Y-m-d H:i:s')
+                    ];
 
-                if (!$getId = Db::name('score_record')->insertGetId($scoreRecord)) {
-                    throw new \think\Exception('添加积分记录失败');
+                    if (!$getId = Db::name('score_record')->insertGetId($scoreRecord)) {
+                        throw new \think\Exception('添加积分记录失败');
+                    }
+
+                    if(!Db::name('sp_spread_record')->where('id',$data['id'])->where('status',1)->update(['status'=>3,'thaw_record_id'=>$getId])){
+                        throw new \think\Exception('分销产品兑付记录表跟新失败');
+                    }
                 }
-
-                if(!Db::name('sp_spread_record')->where('id',$data['id'])->where('status',1)->update(['status'=>2,'thaw_record_id'=>$getId])){
-                    throw new \think\Exception('分销产品兑付记录表跟新失败');
-                }
-
                 Db::commit();
             } catch (\Exception $e) {
                 Db::rollback();
